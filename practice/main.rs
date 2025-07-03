1a7ca8f2da3e2007b969d7d220dc26a1c791e063
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-07-04 02:22:30 +0530

    Add files via upload

--- practice/main.rs
+++ practice/main.rs
@@ -1,9 +1,17 @@
 use std::io;
+
 fn main(){
-	println!("Enter your age :");
-	let mut age=String::new();
+	let mut ageinput=String::new();
+	println!("Enter your age:");
 	let mut input=io::stdin();
-	input.read_line(&mut age);
+	input.read_line(&mut ageinput).expect("Failed to provide the input");
+	let mut age:u32 =ageinput.trim().parse().expect("Invalid Number!");
+
+	if age>=18 {
+		println!("Congrats! you are eligible to vote");
+	}
+	else {
+		println!("Sorry Kid, You'll have to wait..");
+	}
 	
-	println!("Your age is {}",age);
 }
