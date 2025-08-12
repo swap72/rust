757ba23545857bd663a54c9e28b82b3e5591cd3f
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-08-12 17:35:28 +0530

    main.rs

--- practice/main.rs
+++ practice/main.rs
@@ -1,21 +1,39 @@
-// use std::io;
+use std::io;
 
-// fn main(){
-// 	let mut ageinput=String::new();
-// 	println!("Enter your age:");
-// 	let mut input=io::stdin();
-// 	input.read_line(&mut ageinput).expect("Failed to provide the input");
-// 	let mut age:u32 =ageinput.trim().parse().expect("Invalid Number!");
+fn main(){
+    println!("Enter  your Age! ");
+    let mut numInput=String::new();
+    let mut input=io::stdin();
+    input.read_line(&mut numInput);
+    let mut age:u32=numInput.trim().parse().expect("Invalid Input!");
+    if(age<18){
+      println!("Cannot Vote");
+    }
+    else{
+      println!("Can Vote");
+    }
+}
 
-// 	if age>=18 {
-// 		println!("Congrats! you are eligible to vote");
-// 	}
-// 	else {
-// 		println!("Sorry Kid, You'll have to wait..");
-// 	}
-	
-// }
 
+/*
+
+
+Extra string type object creted so that we can run parse() trim() methods onto it and extreact int type value from it.
+
+Because in Rust all standard input is taken as string by default
+
+This is why when our Rust program involves taking an Integer input we create a temporary numInput variable in the beginning of the program itself
+
+Above Program Explained in Steps :
+
+1. Created a temporary numInput first (read above why)
+2. Created and stored io object into input variable
+3. so that we can use object's overloaded read_line() method 
+   which in takes our numInput variable
+4. create our age variable and assign numInput value with chained methods trim and parse, 
+   trim to exclude other characters and parse to extract int value
+
+*/
 
 use std::io;
 
@@ -31,3 +49,4 @@ fn main(){
 
 
 }
+
